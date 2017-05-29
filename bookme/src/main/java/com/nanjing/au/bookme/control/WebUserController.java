4d235714e3eb2a6ad129d1f621017dad09180a02
tonyliuxt <tony.liuxt@gmail.com>
2017-05-29 21:51:33 +1000

    new version for zh_CN

--- bookme/src/main/java/com/nanjing/au/bookme/control/WebUserController.java
+++ bookme/src/main/java/com/nanjing/au/bookme/control/WebUserController.java
@@ -38,14 +38,14 @@ public class WebUserController {
 	@RequestMapping(value="/index", method = RequestMethod.GET)
 	public String index(Locale locale, HttpServletRequest request) {
     	omtlogger.info("/index with language:" + locale + "... Ip address:"+request.getRemoteAddr());
-    	UtilLibs.recordClients(request.getRemoteAddr());
+    	//UtilLibs.recordClients(request.getRemoteAddr());
     	return "index";
 	}
 	
 	@RequestMapping(value="/", method = RequestMethod.GET)
 	public String root(Locale locale,HttpServletRequest request) {
     	omtlogger.info("/ ROOT... with language:" + locale + "... Ip address:"+request.getRemoteAddr());
-    	UtilLibs.recordClients(request.getRemoteAddr());
+    	//UtilLibs.recordClients(request.getRemoteAddr());
     	return "index";
 	}
 	
